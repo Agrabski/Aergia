5fc81505bbd675518dcdadcb7549c049a3732875
Adam Grabski <adam.gr@outlook.com>
2019-05-12 20:12:12 +0200

    fixed nested loops causing a crash

--- Parser/ForeachHeader.cpp
+++ Parser/ForeachHeader.cpp
@@ -1,4 +1,5 @@
 #include "stdafx.h"
+#include <boost/regex.hpp>
 #include "ForeachHeader.hpp"
 #include "RegExLibrar.hpp"
 
@@ -8,17 +9,23 @@ std::vector<ForeachDescriptor> Aergia::Parser::ForeachDescriptor::getForeachDesc
 {
 	std::vector<ForeachDescriptor>result;
 	std::wcmatch matches;
-	static auto regex = std::wregex(foreachRegex, std::wregex::optimize | std::wregex::ECMAScript);
+	static auto regex = boost::wregex(foreachRegex);
 
-	for (auto match = std::wsregex_iterator(text.begin(), text.end(), regex); match != std::wsregex_iterator(); match++)
+	for (auto match = boost::wsregex_iterator(text.begin(), text.end(), regex); match != boost::wsregex_iterator(); match++)
 	{
-		auto m = *match;
+		auto& m = *match;
 		result.emplace_back();
-		result.back()._variableName = m[1].str();
-		result.back()._collectionCallChain = m[2].str();
-		result.back()._foreachBody = m[4].matched ? m[4].str() : m[3].str();
+		result.back()._variableName = std::wstring(m[1].first, m[1].second);
+		result.back()._collectionCallChain = std::wstring( m[2].first, m[2].second );
+		result.back()._foreachBody = m[4].matched ? std::wstring( m[4].first, m[4].second ) : std::wstring( m[3].first, m[3].second );
 		result.back()._positionInInputString = match->position();
 		result.back()._textLength = m[0].str().length();
+		auto t1 = m[1].str();
+		auto t2 = m[2].str();
+		auto t3 = m[3].str();
+		auto t4 = m[4].str();
+		auto t5 = m[5].str();
+		auto t6 = m[6].str();
 	}
 	return result;
 }
