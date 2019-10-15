e10228d51d0305bf9e3e0c7504e07c3db712735f
Adam Grabski <adam.gr@outlook.com>
2019-10-15 09:27:01 +0200

    added application tests

---
+++ ApplicationTests/ApplicationTests.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Aergis/ProjectProcessor.hpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ApplicationTests
+{
+	TEST_CLASS(ApplicationTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
